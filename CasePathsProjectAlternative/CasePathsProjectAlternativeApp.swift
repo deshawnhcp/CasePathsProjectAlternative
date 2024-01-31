801fc97d6e1bcf88103c2d0b17eca26bb1779ed7
DeShawn Jackson <deshawn.jackson@housecallpro.com>
2024-01-31 10:34:34 -0600

    Initial Commit

---
+++ CasePathsProjectAlternative/CasePathsProjectAlternativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasePathsProjectAlternativeApp.swift
+//  CasePathsProjectAlternative
+//
+//  Created by deshawn.jackson on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct CasePathsProjectAlternativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
